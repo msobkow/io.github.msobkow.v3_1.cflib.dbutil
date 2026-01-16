9578da2a98c8a945da001c099e056d4b66afa9be
Mark Sobkow <mark.sobkow@gmail.com>
2026-01-16 05:14:40 -0600

    2026-01-16 05:10 Initial checkin of clean-compiling io.github.msobkow CFLib packages

---
+++ src/main/java/CFLibDbKeyHash384Converter.java
@@ -0,0 +1,49 @@
+/*
+ *	Mark's Code Fractal CFLib DbUtil 3.1 Database Utilities and Types
+ *
+ *	Copyright 2016-2026 Mark Stephen Sobkow
+ *
+ *	This file is part of Mark's Code Fractal CFLib DbUtil.
+ *
+ *	Mark's Code Fractal CFLib DbUtil is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
+ *	Version 3 or later.
+ *
+ *	Mark's Code Fractal CFLib DbUtil is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU Library General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Mark's Code Fractal CFLib DbUtil is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Library General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Library General Public License
+ *	along with Mark's Code Fractal CFLib DbUtil.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ *
+ *	If you wish to modify and use this code without publishing your changes in order to
+ *	tie it to proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
+
+// package io.github.msobkow.v3_1.cflib.dbutil;
+
+import io.github.msobkow.v3_1.cflib.dbutil.CFLibDbKeyHash384;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class CFLibDbKeyHash384Converter implements AttributeConverter<CFLibDbKeyHash384, byte[]> {
+    
+    @Override
+    public byte[] convertToDatabaseColumn(CFLibDbKeyHash384 attribute) {
+        return attribute != null ? attribute.getBytes() : null;
+    }
+
+    @Override
+    public CFLibDbKeyHash384 convertToEntityAttribute(byte[] dbData) {
+        return dbData != null ? new CFLibDbKeyHash384(dbData) : null;
+    }
+}
