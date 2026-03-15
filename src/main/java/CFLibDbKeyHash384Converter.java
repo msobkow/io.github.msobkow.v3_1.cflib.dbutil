3873ef4a37d469bed198e97783ff552bf147f6d9
Mark Sobkow <mark.sobkow@gmail.com>
2026-03-14 20:09:53 -0600

    2026-03-14 20:05 CST Propagate exception clause and license specifics to source code files of the various server.markhome.mcf.v3_1.cflib* packages/jars

--- src/main/java/CFLibDbKeyHash384Converter.java
+++ src/main/java/CFLibDbKeyHash384Converter.java
@@ -1,13 +1,25 @@
 /*
- *	Mark's Code Fractal CFLib DbUtil 3.1 Database Utilities and Types
+ *	Mark's Code Fractal CFLib DbUtil 3.1 Common Library
  *
  *	Copyright 2016-2026 Mark Stephen Sobkow
  *
- *	This file is part of Mark's Code Fractal CFLib DbUtil.
+ *	CFLib DbUtil provides the essential database types and utilities for Code Fractal applications.
+ *
+ *	These files are part of Mark's Code Fractal CFLib DbUtil.
  *
  *	Mark's Code Fractal CFLib DbUtil is available under dual commercial license from
  *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
  *	Version 3 or later.
+ *
+ *	As a special exception, Mark Sobkow gives you permission to link this library
+ *	with independent modules to produce an executable, provided that none of them
+ *	conflict with the intent of the LGPLv3; that is, you are not allowed to invoke
+ *	the methods of this library from non-LGPLv3-compatibly licensed code.  That said,
+ *	code which does not rely on this library is free to specify whatever license its
+ *	authors decide to use. Mark Sobkow specifically rejects the infectious nature of
+ *	the LGPLv3, and considers the mere act of including LGPLv3 modules in an
+ *	executable to be perfectly reasonable given tools like modern Java's single-jar
+ *	deployment options.
  *
  *	Mark's Code Fractal CFLib DbUtil is free software: you can redistribute it and/or
  *	modify it under the terms of the GNU Library General Public License as published by
